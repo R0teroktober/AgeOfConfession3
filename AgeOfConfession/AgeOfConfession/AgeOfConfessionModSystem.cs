030633b2df078ddf16c41bf5dc0af834cf273ef6
R0teroktober <153863464+R0teroktober@users.noreply.github.com>
2026-04-19 18:42:13 +0200

    Add files via upload

---
+++ AgeOfConfession/AgeOfConfession/AgeOfConfessionModSystem.cs
@@ -0,0 +1,34 @@
+﻿using AgeOfConfession;
+using System;
+using Vintagestory.API.Common;
+using Vintagestory.GameContent;
+
+namespace AgeOfConfession
+{
+    public class AgeOfConfessionModSystem : ModSystem
+    {
+        public static ConfessionConfig Config { get; private set; }
+        public override void Start(ICoreAPI api)
+        {
+            base.Start(api);
+           
+            Config = api.LoadModConfig<ConfessionConfig>("confession-config.json");
+            if (Config == null)
+            {
+                Config = new ConfessionConfig()
+                {
+
+       
+                };
+
+                api.StoreModConfig(Config, "ageofconfession.json");
+                api.Logger.Notification("[AgeOfConfession] Default config created.");
+            }
+            else
+            {
+                api.Logger.Notification("[AgeOfConfession] Config loaded successfully.");
+            }
+        }
+    }
+
+}
---
+++ AgeOfConfession/AgeOfConfession/src/ConfessionConfig.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace AgeOfConfession
+{
+    public class ConfessionConfig
+    {
+      
+    }
+}
