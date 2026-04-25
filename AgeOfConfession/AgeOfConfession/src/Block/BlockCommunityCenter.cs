b7d7eceb44ae218984d54491c0436a397f736048
Roteroktober <st_bob_24@gmx.de>
2026-04-25 12:29:30 +0200

    Added: More Community center blocks

--- AgeOfConfession/AgeOfConfession/src/Block/BlockCommunityCenter.cs
+++ AgeOfConfession/AgeOfConfession/src/Block/BlockCommunityCenter.cs
@@ -7,6 +7,35 @@ namespace AgeOfConfession {
 
     public class BlockCommunityCenter : Block
     {
+
+        public override ItemStack[] GetDrops(IWorldAccessor world, BlockPos pos, IPlayer byPlayer, float dropQuantityMultiplier = 1f)
+        {
+            string style = Variant["style"];
+            string firstCodePart = Code.FirstCodePart();
+
+            Block dropBlock = world.GetBlock(new AssetLocation(Code.Domain, $"{firstCodePart}-{style}-unbound-ns"));
+            if (dropBlock == null) { dropBlock = world.GetBlock(new AssetLocation(Code.Domain, $"{firstCodePart}-{style}-unbound-north")); }
+
+            if (dropBlock == null || dropBlock.Id == 0)
+            {
+                return new ItemStack[0];
+            }
+
+            return new[]
+            {
+                new ItemStack(dropBlock)
+            };
+        }
+        public override ItemStack OnPickBlock(IWorldAccessor world,BlockPos pos)
+        {
+            string style = Variant["style"];
+            string firstCodePart = Code.FirstCodePart();
+
+            Block pickBlock = world.GetBlock(new AssetLocation(Code.Domain,$"{firstCodePart}-{style}-unbound-ns"));
+            if (pickBlock == null) { pickBlock = world.GetBlock(new AssetLocation(Code.Domain, $"{firstCodePart}-{style}-unbound-north")); }
+
+            return new ItemStack(pickBlock ?? this);
+        }
         public override void OnBlockRemoved(IWorldAccessor world, BlockPos pos)
         {
             if (world.Api.Side == EnumAppSide.Server)
@@ -22,5 +51,5 @@ namespace AgeOfConfession {
 
             base.OnBlockRemoved(world, pos);
         }
-    }
+      }
 }
